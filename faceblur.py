0e485b1bf95957e8794d61b0958fe3dc8fe256e6
Alexander Heinlein <alexander.heinlein@web.de>
2020-09-25 11:00:30 +0200

    add command line options

--- faceblur.py
+++ faceblur.py
@@ -1,12 +1,14 @@
 '''
 Recognize and blur all faces in photos.
 '''
-import os
-import sys
+import argparse
 import cv2
 import face_recognition
+import os
+import sys
+from shutil import copyfile
 
-def face_blur(src_img, dest_img, zoom_in=1):
+def face_blur(src_img, dest_img, args):
     '''
     Recognize and blur all faces in the source image file, then save as destination image file.
     '''
@@ -17,31 +19,34 @@ def face_blur(src_img, dest_img, zoom_in=1):
     face_locations = []
     photo = face_recognition.load_image_file(src_img)
     # Resize image to  1/zoom_in size for faster face detection processing
-    small_photo = cv2.resize(photo, (0, 0), fx=1/zoom_in, fy=1/zoom_in)
+    small_photo = cv2.resize(photo, (0, 0), fx=1/args.zoom_in, fy=1/args.zoom_in)
 
     # Find all the faces and face encodings in the current frame of video
-    face_locations = face_recognition.face_locations(small_photo, model="cnn")
+    face_locations = face_recognition.face_locations(small_photo, model=args.model, number_of_times_to_upsample=args.upsampling)
 
     if face_locations:
         print("%s:There are %s faces at " % (src_img, len(face_locations)), face_locations)
     else:
         print('%s:There are no any face.' % (src_img))
+        if args.copy:
+            copyfile(src_img, dest_img)
+            print('Original photo has been saved in %s' % dest_img)
         return False
 
     #Blur all face
     photo = cv2.imread(src_img)
     for top, right, bottom, left in face_locations:
         # Scale back up face locations since the frame we detected in was scaled to 1/zoom_in size
-        top *= zoom_in
-        right *= zoom_in
-        bottom *= zoom_in
-        left *= zoom_in
+        top *= args.zoom_in
+        right *= args.zoom_in
+        bottom *= args.zoom_in
+        left *= args.zoom_in
 
         # Extract the region of the image that contains the face
         face_image = photo[top:bottom, left:right]
 
         # Blur the face image
-        face_image = cv2.GaussianBlur(face_image, (21, 21), 0)
+        face_image = cv2.GaussianBlur(face_image, (args.blurr, args.blurr), 0)
 
         # Put the blurred face region back into the frame image
         photo[top:bottom, left:right] = face_image
@@ -49,11 +54,11 @@ def face_blur(src_img, dest_img, zoom_in=1):
     #Save image to file
     cv2.imwrite(dest_img, photo)
 
-    print('Face blurred photo has been save in %s' % dest_img)
+    print('Face blurred photo has been saved in %s' % dest_img)
 
     return True
 
-def blur_all_photo(src_dir, dest_dir):
+def blur_all_photo(src_dir, dest_dir, args):
     '''
     Blur all faces in the source directory photos and copy them to destination directory
     '''
@@ -70,14 +75,25 @@ def blur_all_photo(src_dir, dest_dir):
             if ext == '.jpg':
                 srcfile_path = os.path.join(root, filename)
                 destfile_path = os.path.join(new_root_path, os.path.basename(filename))
-                face_blur(srcfile_path, destfile_path)
+                face_blur(srcfile_path, destfile_path, args)
 
 if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        print('faceblur v1.0.0 (c) telesoho.com')
-        print('Usage:python faceblur <src-image/src-directory> <dest-image/dest-directory>')
+    parser = argparse.ArgumentParser(
+        description='''Recognize and blur all faces in photo. faceblur v1.0.0 (c) telesoho.com''',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('src', help='source image or directory')
+    parser.add_argument('dest', help='destination image or directory')
+    parser.add_argument('-c', '--copy', action="store_true", default=False, help='copy src image to dest even if no faces have been detected')
+    parser.add_argument('-m', '--model', default='cnn', choices=['cnn', 'hog'], help='recognition model, Convolutional Neural Network (CNN) or Histogram of Oriented Gradients (HOG)')
+    parser.add_argument('-b', '--blurr', default=21, type=int, help='gaussian blurr kernel size (the neighbors to be considered), number must be odd')
+    parser.add_argument('-u', '--upsampling', default=1, type=int, help='upsampling factor, higher values increase face recognition rate')
+    parser.add_argument('-z', '--zoom_in', default=1, type=int, help='zoom in factor, higher values increase face detection speed')
+    args = parser.parse_args()
+
+    if args.blurr % 2 == 0:
+        raise argparse.ArgumentTypeError("%s is not an odd int value" % args.blurr)
+
+    if os.path.isfile(args.src):
+        face_blur(args.src, args.dest, args)
     else:
-        if os.path.isfile(sys.argv[1]):
-            face_blur(sys.argv[1], sys.argv[2])
-        else:
-            blur_all_photo(sys.argv[1], sys.argv[2])
+        blur_all_photo(args.src, args.dest, args)
